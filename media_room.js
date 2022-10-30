522f8247682ef15b52f8e7b4c5a406964ba75b22
SpaceRulesGod <81037188+SpaceRulesGod@users.noreply.github.com>
2022-10-30 00:29:17 -0400

    Add files via upload

---
+++ media_room.js
@@ -0,0 +1,27 @@
+img="";
+status="";
+objects=[]; 
+function preload(){
+    img=loadImage('media_room.jpeg');
+}
+function setup(){
+    canvas= createCanvas(640,420);
+    canvas.center();
+    objectDetector = ml5.objectDetector('cocossd', modelLoaded);
+    document.getElementById("stauts").innerHTML = "Status: Detecting object";
+
+}
+function modelLoaded(){
+    console.log("Cocossd is Initiallized");
+    status=true;
+    objectDetector.detect(img, gotResults);
+}
+function gotResults(error,results){
+    if(error){
+        console.log(error);
+    }
+    else{
+        console.log(results);
+        console.log(objects);
+    }
+}
